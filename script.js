67786119e216ea79bb182d4cc494cd3edbb3d64e
Melissa Mitchell <melissa.codingphoenix@gmail.com>
2022-10-23 21:55:08 -0500

    Infinite Scrolling

---
+++ script.js
@@ -0,0 +1,79 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+let count = 5; 
+const apiKey = config.My_API_Key;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        count = 30;
+    }
+}
+
+// Helper functions to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// create elements for links and photos, add to DOM
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create anchor link to Unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        // Create image element for photos
+        const img = document.createElement('img');
+        setAttributes(img, {
+           src: photo.urls.regular,
+           alt: photo.alt_description,
+           title: photo.alt_description, 
+        });
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+        // Put image inside anchor element, then put both inside image container
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+async function getPhotosFromUnsplash() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        //catch error here
+    }
+}
+
+// Check to see if scrolling near bottom of page, then load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotosFromUnsplash();
+    }
+});
+
+// Run function on load
+getPhotosFromUnsplash();
